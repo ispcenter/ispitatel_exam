3c0fadedeb3512b71860817f43a0cbf332b8dbc2
icenter <57789326+ispcenter@users.noreply.github.com>
2022-03-22 10:12:32 +0300

    initial commit

---
+++ questions.js
@@ -0,0 +1,42 @@
+const myQuestions = [
+  {
+    question: "1) Текущая окружная скорость = 250 м/с, температура газа на входе в турбину = 650 ℃. Регулированием каких клапанов можно добиться повышения скорости до 300 м/с?",
+    answers: {
+      a: "6й- и 7й+",
+      b: "7й+ и топливный+",
+      c: "6й+ и топливный+"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "2) Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "3) Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "4) Температура на входе в турбину 700 ℃, окружная скорость 240 м/с. Какой клапан нужно регулировать для получения скорости 250 и температуры 650?",
+    answers: {
+      a: "7й - увеличить подачу воздуха в камеру",
+      b: "7й - уменьшить подачу воздуха в камеру",
+      c: "6й - прикрыть заслонку компрессора",
+      d: "6й - приоткрыть заслонку компрессора",
+      e: "топливный клапан '+'",
+      f: "топливный клапан '-'",
+    },
+    correctAnswer: "a"
+  }
+];
